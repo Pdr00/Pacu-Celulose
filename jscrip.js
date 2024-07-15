95a8f10858bbb350274d0acfccffed9e80d5df60
Pdr0_ <157502308+Pdr00@users.noreply.github.com>
2024-07-15 05:36:02 -0300

    PACU - Upload

---
+++ jscrip.js
@@ -0,0 +1,68 @@
+function alf(){
+    var list = document.querySelector('.troca')
+
+    if (list.style.display === 'none'){
+
+        list.style.display = 'block'
+    }else{
+        list.style.display = 'none'
+    }
+}
+
+function pacu(){
+    location.href = 'senac-canva1.html'
+}
+
+function palito(){
+    location.href = 'senac-canva2.html'
+}
+
+function pahand(){
+    location.href = 'senac-canva3.html'
+}
+
+
+function btnr(){
+    location.reload()
+}
+
+function gasta(){
+    var compre = document.querySelector('.paper')
+
+    compre.style.display = 'block'
+}
+
+function suma(){
+    var compre = document.querySelector('.paper')
+
+    compre.style.display = 'none'
+}
+
+function comprou(){
+    alert('Compra efetuada com sucesso!')
+}
+
+function carrinho(){
+    var numero = document.querySelector('.car-num')
+    numero.style.display = 'block'
+    
+    alert('Compra adicionada ao seu carrinho de compras!')    
+}
+
+function opa(){
+    var carr = document.querySelector('.sai')
+
+    carr.style.display = 'block'
+}
+
+function sumaC(){
+    var carrinho = document.querySelector('.sai')
+
+    carrinho.style.display = 'none'
+}
+
+function semNum(){
+    var num = document.querySelector('.car-num')
+
+    num.style.display = 'none'
+}
